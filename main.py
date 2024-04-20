1493eb1b15310b286015afca2103e4d8c302864b
Anastasiia2001148 <164914414+Anastasiia2001148@users.noreply.github.com>
2024-04-20 21:42:20 +0300

    first task

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("Hello ANA")
+from datetime import datetime
+def get_days_from_today(date):
+   try:
+        date_old = datetime.strptime(date, '%Y-%m-%d').date()
+        current_date = datetime.today().date()
+        if date_old > current_date:
+            count_days = date_old-current_date
+            return print(count_days.days)
+        elif date_old < current_date:
+            count_days = current_date - date_old
+            return print(-count_days.days)
+        else:
+            return print('Dates are equal')
+   except ValueError:
+    return print("Does not match format")
 
-print("Hello MURAT")
+get_days_from_today('2024-06-09')
